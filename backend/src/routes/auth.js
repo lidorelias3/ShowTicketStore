54bd9639d77f5654cc138e15bd0bad163158ef19
lidorelias3 <41958137+lidorelias3@users.noreply.github.com>
2024-09-25 18:48:03 +0300

    Revert "Fix responses"

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -8,11 +8,13 @@ const router = express.Router();
 
 const handleResponse = require('../utils/responseHandler');
 
+
 // Middleware for validating user input
 const RegisterValidator = [
     body('email').isEmail().withMessage('Must be a valid email address'),
     body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
 ];
+
 
 router.post('/login', RegisterValidator, async (req, res) => {
     const errors = validationResult(req);
@@ -42,6 +44,9 @@ router.post('/login', RegisterValidator, async (req, res) => {
     }
 });
 
+
+
+
 router.post('/register', RegisterValidator, async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -54,7 +59,7 @@ router.post('/register', RegisterValidator, async (req, res) => {
         // Check if the email already exists
         const existingUser = await User.findOne({ email });
         if (existingUser) {
-            return handleResponse(res, 409, false, "Email already in use");
+            return res.status(409).json({ message: "Email already in use" });
         }
 
         // Create a new user
@@ -67,10 +72,10 @@ router.post('/register', RegisterValidator, async (req, res) => {
 
         // Save the user to the database
         await newUser.save();
-        return handleResponse(res, 201, true, "User registered successfully");
+        res.status(201).json({ message: "User registered successfully" });
 
     } catch (error) {
-        return handleResponse(res, 500, false, "Error registering user", error.message);
+        res.status(500).json({ message: "Error registering user", error: error.message });
     }
 });
 
