efabe9ba14a985091691aceab283869f51888c9c
nadav <nadavmazooz123@gmail.com>
2024-10-06 13:31:23 +0300

    google map added without dynamically show the venue name

--- frontend/src/app/components/show/show.component.ts
+++ frontend/src/app/components/show/show.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Route, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Event } from 'src/app/models/event.model';
 import { EventsService } from 'src/app/services/events.service';
 import { TicketsService } from 'src/app/services/tickets.service';
@@ -7,45 +7,66 @@ import { TicketsService } from 'src/app/services/tickets.service';
 @Component({
   selector: 'app-show',
   templateUrl: './show.component.html',
-  styleUrls: ['./show.component.scss']
+  styleUrls: ['./show.component.scss'],
 })
-export class ShowComponent {
+export class ShowComponent implements OnInit {
   id: string;
   show: Event;
 
   selectedType: string = '';
-  amount = 1
+  amount = 1;
 
-  constructor(private route: ActivatedRoute, private eventsService: EventsService,
-    private ticketsService: TicketsService, private router: Router
+  // Map-related variables
+  center: google.maps.LatLngLiteral; // Google Maps center
+  zoom = 15; // Zoom level for the map
+
+  constructor(
+    private route: ActivatedRoute,
+    private eventsService: EventsService,
+    private ticketsService: TicketsService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.id = params['id'] || 0;
-    }).unsubscribe();
+    // Get the event by ID
+    this.route.params
+      .subscribe((params) => {
+        this.id = params['id'] || 0;
+      })
+      .unsubscribe();
 
-    this.eventsService.getEventByID(this.id).subscribe(res => {
+    this.eventsService.getEventByID(this.id).subscribe((res) => {
       this.show = res.message;
-    })
+
+      // Set the map center (either static coordinates or fetched dynamically)
+      this.setMapCenter();
+    });
+  }
+
+  // Set static coordinates for now (replace with actual coordinates)
+  setMapCenter() {
+    this.center = {
+      lat: 40.73061, // Example latitude (replace with actual)
+      lng: -73.935242, // Example longitude (replace with actual)
+    };
   }
 
   addToCart() {
-    if (this.selectedType == '') {
-      alert("אנא בחר סוג כרטיס")
-      return
+    if (this.selectedType === '') {
+      alert('אנא בחר סוג כרטיס');
+      return;
     }
 
     this.ticketsService.addToCart(
       this.show,
-      this.show.tickets.filter(it => it.ticketType == this.selectedType)[0],
+      this.show.tickets.filter((it) => it.ticketType == this.selectedType)[0],
       this.amount
-    )
+    );
 
-    this.router.navigate([""])
+    this.router.navigate(['']);
   }
 
   increaseAmount(by: number) {
-    this.amount += 1 * by
+    this.amount += by;
   }
 }
