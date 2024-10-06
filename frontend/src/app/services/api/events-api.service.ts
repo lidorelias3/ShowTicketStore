804ef849faaa9c9b781aa0aa1b37ffc5bfee9e9b
Ido Czes <idoczesler@gmail.com>
2024-10-07 00:25:18 +0300

    Add authorization header to requests and modified user service to conform with JWT api

--- frontend/src/app/services/api/events-api.service.ts
+++ frontend/src/app/services/api/events-api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Event } from 'src/app/models/event.model';
 import { Observable, Subject } from 'rxjs';
-import * as $ from 'jquery';
+import { authenticatedAjax } from './ajax.util';
 
 @Injectable({
   providedIn: 'root',
@@ -10,44 +10,61 @@ export class EventAPIService {
 
   getAllEvents(): Observable<any> {
     var subject = new Subject<any>()
-    $.get('http://localhost:3000/api/event/',
-      function (data, _) {
+    authenticatedAjax({
+      type: 'GET',
+      url: 'http://localhost:3000/api/event/',
+      success: function (data: any) {
+        subject.next(data)
+      },
+      error: function(data: any) {
         subject.next(data)
       }
-    );
+    });
 
     return subject.asObservable()
   }
 
   getById(id: String): Observable<any> {
     var subject = new Subject<any>()
-    $.get('http://localhost:3000/api/event/' + id,
-      function (data, _) {
+    authenticatedAjax({
+      type: 'GET',
+      url: 'http://localhost:3000/api/event/' + id,
+      success: function (data: any) {
+        subject.next(data)
+      },
+      error: function(data: any) {
         subject.next(data)
       }
-    );
+    });
 
     return subject.asObservable()
   }
 
   createEvent(event: Event): Observable<any> {
     var subject = new Subject<any>()
-    $.post('http://localhost:3000/api/event', event,
-      function (data, _) {
+    authenticatedAjax({
+      type: 'POST',
+      url: 'http://localhost:3000/api/event',
+      data: event,
+      contentType: "application/json",
+      success: function (data: any) {
+        subject.next(data)
+      },
+      error: function(data: any) {
         subject.next(data)
       }
-    );
+    });
 
     return subject.asObservable()
   }
 
   deleteEvent(name: string) {
     var subject = new Subject<any>()
-    $.ajax({
+   authenticatedAjax({
       url: `http://localhost:3000/api/event/${name}`  ,
       type: 'DELETE',
       async: true,
-      success: function (result) {
+      success: function (result: any) {
         subject.next(result)
       }
     });
@@ -56,16 +73,16 @@ export class EventAPIService {
 
   updateExistingEvent(existEventName: string, event: Event): Observable<any> {
     var subject = new Subject<any>()
-    $.ajax({
+    authenticatedAjax({
       type: "PUT",
       url: `http://localhost:3000/api/event/${encodeURI(existEventName)}`,
       contentType: "application/json",
       data: JSON.stringify(event),
       async: true,
-      success: function (data) {
+      success: function (data: any) {
         subject.next(data)
       },
-      error: function(data) {
+      error: function(data: any) {
         subject.next(data)
       }
     });
