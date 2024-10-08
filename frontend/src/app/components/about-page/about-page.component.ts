abf5b948b0761afbb9d2dc5e002867b4eafa6b65
Ido Czes <idoczesler@gmail.com>
2024-10-08 17:37:45 +0300

    Add about page

---
+++ frontend/src/app/components/about-page/about-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './about-page.component.html',
+  styleUrls: ['./about-page.component.scss']
+})
+export class AboutPageComponent {
+
+}
